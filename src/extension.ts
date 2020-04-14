84ff2fd0fe98d6e74fa7942854bc638995e4d3d0
raushan2016 <raushan2016@outlook.com>
2020-04-14 06:16:27 +0000

    Cleaning up the code

--- src/extension.ts
+++ src/extension.ts
@@ -11,87 +11,71 @@ export async function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-k8s-tree" is now active!');
-	
+
 	const clusterExplorer = await k8s.extension.clusterExplorer.v1;
-    if (!clusterExplorer.available) {
-        vscode.window.showErrorMessage(`ClusterExplorer not available.`);
-        return;
-    }
-
-    const kubectl = await k8s.extension.kubectl.v1;
-    if (!kubectl.available) {
-        vscode.window.showErrorMessage(`kubectl not available.`);
-        return;
-    }
-
-	clusterExplorer.api.registerNodeContributor(
-		clusterExplorer.api.nodeSources.groupingFolder("MIR", undefined,
-		clusterExplorer.api.nodeSources.groupingFolder("Orchestrator", undefined,
-			clusterExplorer.api.nodeSources.resourceFolder("ServiceEndpoint", "ServiceEndpoints", "ServiceEndpoint", "serviceendpoints", ),
-			clusterExplorer.api.nodeSources.resourceFolder("ManagedInferenceresource", "ManagedInferenceresources","managedinferenceresource.orchestrator.mir.azureml.k8s.io", "managedinferenceresource.orchestrator.mir.azureml.k8s.io", ),
-			clusterExplorer.api.nodeSources.resourceFolder("ModelSource", "ModelSources", "modelsource.orchestrator.mir.azureml.k8s.io", "modelsource.orchestrator.mir.azureml.k8s.io", ),
-			clusterExplorer.api.nodeSources.resourceFolder("ModelService", "ModelServices", "modelservice.orchestrator.mir.azureml.k8s.io", "modelservice.orchestrator.mir.azureml.k8s.io")
-	),
-	clusterExplorer.api.nodeSources.groupingFolder("Instance", undefined,
-	clusterExplorer.api.nodeSources.resourceFolder("ManagedInferenceresource", "ManagedInferenceresources", "ManagedInferenceresource.instance.mir.azureml.k8s.io", "managedinferenceresource.instance.mir.azureml.k8s.io", ),
-	clusterExplorer.api.nodeSources.resourceFolder("ModelSource", "ModelSources", "ModelSource.instance.mir.azureml.k8s.io", "modelsource.instance.mir.azureml.k8s.io", ),
-	clusterExplorer.api.nodeSources.resourceFolder("ModelService", "ModelServices", "ModelService.instance.mir.azureml.k8s.io", "modelservice.instance.mir.azureml.k8s.io")
-	)
-	).at(undefined));
-
-let disposable = vscode.commands.registerCommand('vscode-k8s-tree.show',(resource)=>{
-	treeView(resource);
-});
-
-context.subscriptions.push(disposable);
- //explorer.api.registerNodeContributor(new FileSystemNodeContributor(kubectl.api));
-
-}
-
-async function treeView(resource: any){
-	const clusterExplorer = await k8s.extension.clusterExplorer.v1;
-    if (!clusterExplorer.available) {
-        vscode.window.showErrorMessage(`ClusterExplorer not available.`);
-        return;
+	if (!clusterExplorer.available) {
+		vscode.window.showErrorMessage(`ClusterExplorer not available.`);
+		return;
 	}
 
 	const kubectl = await k8s.extension.kubectl.v1;
-    if (!kubectl.available) {
-        vscode.window.showErrorMessage(`kubectl not available.`);
-        return;
-    }
-	
-	const treeNode = clusterExplorer.api.resolveCommandTarget(resource);
-	if(!treeNode || treeNode.nodeType !== 'resource'){
-		vscode.window.showErrorMessage(`resource name not available.`);
-		return		
-	}
-	const name = treeNode.name;
-	const sr = await kubectl.api.invokeCommand('version');
-	if (!sr || sr.code !== 0) {
-		vscode.window.showErrorMessage(sr ? sr.stderr : 'oof');
+	if (!kubectl.available) {
+		vscode.window.showErrorMessage(`kubectl not available.`);
 		return;
 	}
-	vscode.window.showInformationMessage(sr.stdout);
 
-	//const obj = await kubectl.api.invokeCommand(`tree -A ${treeNode.resourceKind.manifestKind} ${treeNode.name}`);
-	const obj = await kubectl.api.invokeCommand(`tree --help`);
-		if(!obj || obj.code != 0)
+	let disposable = vscode.commands.registerCommand('vscode-k8s-tree.show', (resource) => {
+		treeView(resource);
+	});
+
+	context.subscriptions.push(disposable);
+
+}
+
+async function treeView(resource: any) {
+
+	const clusterExplorer = await k8s.extension.clusterExplorer.v1;
+	if (!clusterExplorer.available) {
+		vscode.window.showErrorMessage(`ClusterExplorer not available.`);
+		return;
+	}
+
+	const kubectl = await k8s.extension.kubectl.v1;
+	if (!kubectl.available) {
+		vscode.window.showErrorMessage(`kubectl not available.`);
+		return;
+	}
+
+	const rootNode = clusterExplorer.api.resolveCommandTarget(resource);
+	if (!rootNode || rootNode.nodeType !== 'resource') {
+		vscode.window.showErrorMessage(`resource name not available.`);
+		return;
+	}
+
+	const rootObjectName = rootNode.name;
+	const rootObjectKind = rootNode.resourceKind.manifestKind;
+
+	const commandResult = await kubectl.api.invokeCommand(`tree -A ${rootObjectKind} ${rootObjectName}`);
+
+	if (!commandResult || commandResult.code !== 0) {
+		if(commandResult?.stderr.includes("unknown command \"tree\" for \"kubectl\""))
 		{
-			vscode.window.showErrorMessage(obj?.stderr ?? `Unable to get the resource ${treeNode.resourceKind}/${treeNode.name}`)
-			return
+			vscode.window.showErrorMessage(`Make sure you have installed kubectl plugin \"tree\". Run \"kubectl krew install tree\", More details https://github.com/ahmetb/kubectl-tree`);
+			return;
 		}
+		vscode.window.showErrorMessage(commandResult?.stderr ?? `Unable to get the resource ${rootObjectKind}/${rootObjectName}`);
+		return;
+	}
 
-	vscode.window.showInformationMessage("tree view");
 	// Create and show a new webview
 	const panel = vscode.window.createWebviewPanel(
-	 'catCoding', // Identifies the type of the webview. Used internally
-	 `Tree View ${treeNode.resourceKind}/${treeNode.name}`, // Title of the panel displayed to the user
-	 vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-	 {} // Webview options. More on these later.
+		'treeview', // Identifies the type of the webview. Used internally
+		`Tree View ${rootObjectKind}/${rootObjectName}`, // Title of the panel displayed to the user
+		vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+		{} // Webview options. More on these later.
 	);
-	   // And set its HTML contentg
-	   panel.webview.html = getWebviewContent(name,obj.stdout);
+	// And set its HTML contentg
+	panel.webview.html = getWebviewContent(rootObjectName, commandResult.stdout);
 }
 
 function getWebviewContent(name: string, treeNode: any) {
@@ -104,10 +88,10 @@ function getWebviewContent(name: string, treeNode: any) {
   </head>
   <body>
 	 <b>${name}</b>
-	 <p>${treeNode}</p>
+	 <pre>${treeNode}</pre>
   </body>
   </html>`;
-  }
+}
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
